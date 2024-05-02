5df10e83e4841dd09a2a3aa06e85b5b75a288615
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2024-05-02 14:35:31 -0500

    Conexión base de datos AWS

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-module.exports={
-    app:{
+module.exports = {
+    app: {
         port: process.env.PORT || 4000
     },
-    mysql:{
-        host: process.env.MYSQL_HOST || 'viaduct.proxy.rlwy.net',
-        user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || 'RVnlJWcAvSamyUKKsCUnJLqeGcPeKcFr',
-        database: process.env.MYSQL_DB || 'railway',
-        port: process.env.MYSQL_PORT || 35504
+    mysql: {
+        host: process.env.MYSQL_HOST || "botero-db.cp4yeku0obx7.us-east-1.rds.amazonaws.com",
+        port: process.env.MYSQL_PORT || 3306,
+        user: process.env.MYSQL_USER || "admin",
+        password: process.env.MYSQL_PASSWORD || "Admin123",
+        database: process.env.MYSQL_DB || "BoteroDB"
     }
 }
--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,32 +2,31 @@ const mysql = require('mysql2');
 const config = require('../../config');
 
 const dbConfig = {
-    host:config.mysql.host,
-    user:config.mysql.user,
-    password:config.mysql.password,
-    database:config.mysql.database,
-    port:config.mysql.sqlport,
+    host: config.mysql.host,
+    port: config.mysql.port,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
 }
 
 let conexion;
 
-const connection = "mysql://root:RVnlJWcAvSamyUKKsCUnJLqeGcPeKcFr@viaduct.proxy.rlwy.net:35504/railway";
-function conexionMySql(){
-    conexion = mysql.createConnection(connection);
-    conexion.connect((err)=>{
-        if(err){
+function conexionMySql() {
+    conexion = mysql.createConnection(dbConfig);
+    conexion.connect((err) => {
+        if (err) {
             console.log('[db error]', err);
             setTimeout(conexionMySql, 200);
 
-        }else{
+        } else {
             console.log('DB Conectada');
         }
     });
-    conexion.on('error', err =>{
+    conexion.on('error', err => {
         console.log('[db error]', err);
-        if(err.code === 'PROTOCOL_CONECTION_LOST'){
+        if (err.code === 'PROTOCOL_CONECTION_LOST') {
             conexionMySql();
-        }else{
+        } else {
             throw err;
         }
     });
