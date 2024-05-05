f374199e895d85008f1e37ef9b1a37496dc67d0e
Erik Molina <90419030+Biacuya@users.noreply.github.com>
2024-05-05 18:27:47 -0500

    addUser

--- src/module/user/ServiceUser.js
+++ src/module/user/ServiceUser.js
@@ -1,4 +1,5 @@
-const connection = require("../../database/MySql")
+const connection = require("../../database/MySql");
+const { error } = require("../../red/respuestas");
 
 function searhAllUsers(table) {
     return new Promise((resolve, reject) => {
@@ -8,12 +9,26 @@ function searhAllUsers(table) {
     });
 }
 
-function searhOneUser(table, id, nickname){
-    return new Promise((resolve, reject) =>{
-        connection.query(`SELCT * FROM ${table} WHERE id_user = ${id} OR `)
+function searhOneUser(table, id, nickname) {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELCT * FROM ${table} WHERE id_user = ${id}`)
     })
 }
 
+function addUser(table, data) {
+    const values = Object.keys(data).map(key => `'${data[key]}'`).join(",");
+
+    const query = `INSERT INTO ${table} (${Object.keys(data).join(",")}) SELECT id_person, ${values} FROM person WHERE person_type = "Usuario"`;
+
+    return new Promise((resolve, reject) => {
+        connection.query(query, (error, result) => {
+            return error ? reject(error) : resolve(result);
+        });
+    });
+}
+
 module.exports = {
-    searhAllUsers
+    searhAllUsers,
+    searhOneUser,
+    addUser
 }
